75bf93fdd36d3a3173ed2b3ed725012afdec07e9
Chris Metcalf <chris@chrismetcalf.net>
2018-05-14 19:10:46 -0700

    Only show updates when needed, duh

--- Dev/Homebrew/brew-updates.1h.sh
+++ Dev/Homebrew/brew-updates.1h.sh
@@ -18,9 +18,12 @@ UPDATES=$(comm -13 <(for X in "${PINNED[@]}"; do echo "${X}"; done) <(for X in "
 
 UPDATE_COUNT=$(echo "$UPDATES" | grep -c '[^[:space:]]');
 
-echo "↑$UPDATE_COUNT | dropdown=false"
-echo "---";
-if [ -n "$UPDATES" ]; then
-  echo "Upgrade all | bash=/usr/local/bin/brew param1=upgrade terminal=false refresh=true"
-  echo "$UPDATES" | awk '{print $0 " | terminal=false refresh=true bash=/usr/local/bin/brew param1=upgrade param2=" $1}'
+if [ "$UPDATE_COUNT" -gt "0" ]; then
+  echo "↑$UPDATE_COUNT | dropdown=false"
+  echo "---";
+  if [ -n "$UPDATES" ]; then
+    echo "Upgrade all | bash=/usr/local/bin/brew param1=upgrade terminal=false refresh=true"
+    echo "$UPDATES" | awk '{print $0 " | terminal=false refresh=true bash=/usr/local/bin/brew param1=upgrade param2=" $1}'
+  fi
 fi
+
